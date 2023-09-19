6a2d432f58abb0d210c62caa15dbd91e90350e27
Manuel Fellner <mfellner@student.tgm.ac.at>
2023-09-19 16:32:43 +0200

    feat(simulation): optimized code for generating random products

--- src/main/java/tradearea/warehouse/WarehouseSimulation.java
+++ src/main/java/tradearea/warehouse/WarehouseSimulation.java
@@ -4,95 +4,62 @@ import tradearea.model.Product;
 import tradearea.model.WarehouseData;
 
 import java.util.UUID;
+import java.util.Random;
 
 public class WarehouseSimulation {
 
-	String[] productNames = {
-			"Milch",
-			"Eier",
-			"Brot",
-			"Reis",
-			"Nudeln",
-			"Kartoffeln",
-			"Tomaten",
-			"Bananen",
-			"Äpfel",
-			"Hühnchen",
-			"Rindfleisch",
-			"Lachs",
-			"Butter",
-			"Käse",
-			"Joghurt"
+	private static final String[] productNames = {
+			"Milch", "Eier", "Brot", "Reis", "Nudeln", "Kartoffeln", "Tomaten",
+			"Bananen", "Äpfel", "Hühnchen", "Rindfleisch", "Lachs", "Butter",
+			"Käse", "Joghurt"
 	};
 
-	String[] productCategories = {
-			"Milchprodukte",
-			"Eier",
-			"Brot und Backwaren",
-			"Getreide und Reis",
-			"Nudeln",
-			"Gemüse",
-			"Obst",
-			"Obst",
-			"Obst",
-			"Fleisch",
-			"Fleisch",
-			"Fisch",
-			"Milchprodukte",
-			"Milchprodukte",
-			"Milchprodukte"
+	private static final String[] productCategories = {
+			"Milchprodukte", "Eier", "Brot und Backwaren", "Getreide und Reis",
+			"Nudeln", "Gemüse", "Obst", "Obst", "Obst", "Fleisch", "Fleisch",
+			"Fisch", "Milchprodukte", "Milchprodukte", "Milchprodukte"
 	};
 
-	private double getRandomDouble( int inMinimum, int inMaximum ) {
+	private static final Random random = new Random();
 
-		double number = ( Math.random() * ( (inMaximum-inMinimum) + 1 )) + inMinimum; 
-		double rounded = Math.round(number * 100.0) / 100.0; 
-		return rounded;
-		
+	private static double getRandomDouble(int inMinimum, int inMaximum) {
+		double number = random.nextDouble() * (inMaximum - inMinimum + 1) + inMinimum;
+		return Math.round(number * 100.0) / 100.0;
 	}
 
-	private int getRandomInt( int inMinimum, int inMaximum ) {
-
-		double number = ( Math.random() * ( (inMaximum-inMinimum) + 1 )) + inMinimum; 
-		Long rounded = Math.round(number); 
-		return rounded.intValue();
-
+	private static int getRandomInt(int inMinimum, int inMaximum) {
+		return random.nextInt(inMaximum - inMinimum + 1) + inMinimum;
 	}
 
-	private int[] getFourRandomNumbers()	{
-		if (this.productNames.length == this.productCategories.length)	{
-			int firstRandomNumber = getRandomInt(0, this.productNames.length-1);
-			int secondRandomNumber = getRandomInt(0, this.productNames.length-1);
-			int thirdRandomNumber = getRandomInt(0, this.productNames.length-1);
-			int fourthRandomNumber = getRandomInt(0, this.productNames.length-1);
-			return new int[]{firstRandomNumber, secondRandomNumber, thirdRandomNumber, fourthRandomNumber};
-		}
-		return new int[]{0, 0, 0, 0};
+	private static int[] getFourRandomNumbers() {
+		int firstRandomNumber = getRandomInt(0, productNames.length - 1);
+		int secondRandomNumber = getRandomInt(0, productNames.length - 1);
+		int thirdRandomNumber = getRandomInt(0, productNames.length - 1);
+		int fourthRandomNumber = getRandomInt(0, productNames.length - 1);
+		return new int[]{firstRandomNumber, secondRandomNumber, thirdRandomNumber, fourthRandomNumber};
 	}
 
-	private Product[] getFourProductData()	{
+	private static Product[] getFourProductData() {
 		int[] randomNumbers = getFourRandomNumbers();
 		Product[] products = new Product[4];
-		products[0] = new Product(UUID.randomUUID(), this.productNames[randomNumbers[0]], this.productCategories[randomNumbers[0]], getRandomInt(75, 200), "1KG/Packung");
-		products[1] = new Product(UUID.randomUUID(), this.productNames[randomNumbers[1]], this.productCategories[randomNumbers[1]], getRandomInt(1000, 2500), "1L/Packung");
-		products[2] = new Product(UUID.randomUUID(), this.productNames[randomNumbers[2]], this.productCategories[randomNumbers[2]], getRandomInt(2000, 7300), "3KG/Packung");
-		products[3] = new Product(UUID.randomUUID(), this.productNames[randomNumbers[3]], this.productCategories[randomNumbers[3]], getRandomInt(100, 1000), "500ML/Packung");
+		for (int i = 0; i < 4; i++) {
+			products[i] = new Product(UUID.randomUUID(), productNames[randomNumbers[i]],
+					productCategories[randomNumbers[i]], getRandomInt(75, 200),
+					i == 1 ? "1L/Packung" : (i == 2 ? "3KG/Packung" : "500ML/Packung"));
+		}
 		return products;
 	}
-	
-	public WarehouseData getData(UUID inID) {
-		
+
+	public static WarehouseData getData(UUID inID) {
 		WarehouseData data = new WarehouseData();
 		// parse String into UUID object, we currently want a static id
 		data.setWarehouseID(inID);
-		data.setWarehouseName( "Linz Bahnhof" );
+		data.setWarehouseName("Linz Bahnhof");
 		data.setWarehouseCity("Linz");
 		data.setWarehouseAddress("WhoKnows Straße 12");
-		data.setWarehouseCountry("Autria");
+		data.setWarehouseCountry("Austria");
 		data.setWarehousePostalCode(4000);
 		data.setProductData(getFourProductData());
-
 		return data;
 	}
-
 }
